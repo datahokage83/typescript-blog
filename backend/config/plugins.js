3c224dc1697abfc1fe633bf878dd8e6c9dfa7147
DeepakBaligar <deepakbaligar-83@users.noreply.github.com>
2025-11-21 22:51:17 +0530

    Perk

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,3 +1,27 @@
+// module.exports = ({ env }) => ({
+//   email: {
+//     config: {
+//       provider: "nodemailer",
+//       providerOptions: {
+//         host: "smtp.gmail.com",
+//         port: 465,
+//         secure: true,
+//         auth: {
+//           user: env("SMTP_USER"),
+//           pass: env("SMTP_PASS"),
+//         },
+//       },
+//       settings: {
+//         defaultFrom: env("SMTP_USER"),
+//         defaultReplyTo: env("SMTP_USER"),
+//       },
+//     },
+//   },
+//   "email-designer": {
+//     enabled: true,
+//   },
+// });
+
 module.exports = ({ env }) => ({
   email: {
     config: {
@@ -17,7 +41,24 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
   "email-designer": {
     enabled: true,
+  },
+
+  // 🔽 ADD THIS BELOW
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
   },
 });
